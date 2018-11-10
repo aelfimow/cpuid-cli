43236345a6a2780e0158489a98670480f6228c98
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-10 22:48:38 +0100

    Added helper class

---
+++ src/bit_extractor.cpp
@@ -0,0 +1,38 @@
+#include <bitset>
+#include <utility>
+
+#include "bit_extractor.h"
+
+
+bit_extractor::bit_extractor(size_t value) :
+    m_value { value }
+{
+}
+
+bit_extractor::~bit_extractor()
+{
+}
+
+size_t bit_extractor::extract(size_t from, size_t to) const
+{
+    std::bitset<64> result { 0 };
+    std::bitset<64> value { m_value };
+
+    size_t pos = 0;
+
+    if (from < to)
+    {
+        std::swap(from, to);
+    }
+
+    size_t const end_pos = from - to + 1;
+    size_t const offset = to;
+
+    while (pos < end_pos)
+    {
+        result[pos] = value[pos + offset];
+        ++pos;
+    }
+
+    return result.to_ullong();
+}
---
+++ src/bit_extractor.h
@@ -0,0 +1,21 @@
+#ifdef BIT_EXTRACTOR_H
+#error Already included
+#else
+#define BIT_EXTRACTOR_H
+
+class bit_extractor
+{
+    public:
+        explicit bit_extractor(size_t value);
+        ~bit_extractor();
+
+        size_t extract(size_t from, size_t to) const;
+
+    private:
+        size_t const m_value;
+
+    public:
+        bit_extractor() = delete;
+};
+
+#endif
