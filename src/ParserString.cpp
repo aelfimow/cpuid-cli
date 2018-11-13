c8258a0701fc3302979cf434791fbf3b235771a5
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-13 21:01:15 +0100

    Added helper class ParserString

---
+++ src/ParserString.cpp
@@ -0,0 +1,22 @@
+#include <sstream>
+
+#include "ParserString.h"
+
+
+ParserString::ParserString(std::string const str, size_t value) :
+    m_str { }
+{
+    std::stringstream ss;
+    ss << str;
+    ss << value;
+    m_str.append(ss.str());
+}
+
+ParserString::~ParserString()
+{
+}
+
+std::string ParserString::str() const
+{
+    return m_str;
+}
---
+++ src/ParserString.h
@@ -0,0 +1,21 @@
+#ifdef PARSER_STRING_H
+#error Already included
+#else
+#define PARSER_STRING_H
+
+class ParserString
+{
+    public:
+        explicit ParserString(std::string const str, size_t value);
+        ~ParserString();
+
+        std::string str() const;
+
+    private:
+        std::string m_str;
+
+    public:
+        Parser_0_0() = delete;
+};
+
+#endif
