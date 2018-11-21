215547a83742d8c96c32e6cfcbe0b1fe5b37d84b
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-21 22:26:57 +0100

    Parser_D_1: implemented parser

--- src/Parser_D_1.cpp
+++ src/Parser_D_1.cpp
@@ -37,19 +37,59 @@ parse_result_t Parser_D_1::parse() const
 void Parser_D_1::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    std::map<size_t, std::string> const table
+    {
+        { 0, "XSAVEOPT is available" },
+        { 1, "Supports XSAVEC and the compacted form of XRSTOR" },
+        { 2, "Supports XGETBV with ECX = 1" },
+        { 3, "Supports XSAVES/XRSTORS and IA32_XSS" }
+    };
+
+    for (auto &t: table)
+    {
+        if (extr.extract(t.first))
+        {
+            m_result.push_back(t.second);
+        }
+    }
 }
 
 void Parser_D_1::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "The size in bytes of the XSAVE area containing all states enabled by XCRO | IA32_XSS",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_D_1::parseRCX(size_t value)
 {
-    bit_extractor extr { value };
+    std::bitset<32> bin_value { value };
+
+    ParserString pstr
+    {
+        "Lower 32 bits of the IA32_XSS MSR",
+        bin_value.to_string()
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_D_1::parseRDX(size_t value)
 {
-    bit_extractor extr { value };
+    std::bitset<32> bin_value { value };
+
+    ParserString pstr
+    {
+        "Upper 32 bits of the IA32_XSS MSR",
+        bin_value.to_string()
+    };
+
+    m_result.push_back(pstr.str());
 }
