2e1396e3b2494daa5e7c4883bd669834174e0125
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-22 22:47:14 +0100

    Parser_F_0: created

---
+++ src/Parser_F_0.h
@@ -0,0 +1,31 @@
+#ifdef PARSER_F_0_H
+#error Already included
+#else
+#define PARSER_F_0_H
+
+class cpuid_response;
+
+class Parser_F_0 : public IParser
+{
+    public:
+        explicit Parser_F_0(cpuid_response const &data);
+        ~Parser_F_0();
+
+        parse_result_t parse() const override final;
+
+    private:
+        parse_result_t m_result;
+        IParser *m_next;
+
+        void parseRAX(size_t value);
+        void parseRBX(size_t value);
+        void parseRCX(size_t value);
+        void parseRDX(size_t value);
+
+    public:
+        Parser_F_0() = delete;
+        Parser_F_0(Parser_F_0 const &) = delete;
+        Parser_F_0 &operator=(Parser_F_0 const &) = delete;
+};
+
+#endif
