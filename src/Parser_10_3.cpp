f3851dfb05fe7b6c5c1f7adabbf146ea5b2153b6
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-24 22:48:58 +0100

    Parser_10_3: created

---
+++ src/Parser_10_3.cpp
@@ -0,0 +1,77 @@
+#include "IParser.h"
+#include "Parser_10_3.h"
+#include "bit_extractor.h"
+#include "cpuid_response.h"
+#include "ParserString.h"
+
+
+Parser_10_3::Parser_10_3(cpuid_response const &data) :
+    m_result { },
+    m_next { nullptr }
+{
+    bool response_ok = (3 == data.RCX_Command());
+
+    if (response_ok)
+    {
+        parseRAX(data.RAX());
+        parseRBX(data.RBX());
+        parseRCX(data.RCX());
+        parseRDX(data.RDX());
+    }
+}
+
+Parser_10_3::~Parser_10_3()
+{
+    delete m_next;
+}
+
+parse_result_t Parser_10_3::parse() const
+{
+    if (m_next != nullptr)
+    {
+        return m_next->parse();
+    }
+
+    return m_result;
+}
+
+void Parser_10_3::parseRAX(size_t value)
+{
+    bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Maximum MBA throttling value supported for ResID 3",
+        1 + extr.extract(11, 0)
+    };
+
+    m_result.push_back(pstr.str());
+}
+
+void Parser_10_3::parseRBX(size_t value)
+{
+    bit_extractor extr { value };
+}
+
+void Parser_10_3::parseRCX(size_t value)
+{
+    bit_extractor extr { value };
+
+    if (extr.extract(2))
+    {
+        m_result.push_back("Response of the delay values is linear");
+    }
+}
+
+void Parser_10_3::parseRDX(size_t value)
+{
+    bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Highest COS number supported for ResID 3",
+        extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
+}
---
+++ src/Parser_10_3.h
@@ -0,0 +1,31 @@
+#ifdef PARSER_10_3_H
+#error Already included
+#else
+#define PARSER_10_3_H
+
+class cpuid_response;
+
+class Parser_10_3 : public IParser
+{
+    public:
+        explicit Parser_10_3(cpuid_response const &data);
+        ~Parser_10_3();
+
+        parse_result_t parse() const override final;
+
+    private:
+        parse_result_t m_result;
+        IParser *m_next;
+
+        void parseRAX(size_t value);
+        void parseRBX(size_t value);
+        void parseRCX(size_t value);
+        void parseRDX(size_t value);
+
+    public:
+        Parser_10_3() = delete;
+        Parser_10_3(Parser_10_3 const &) = delete;
+        Parser_10_3 &operator=(Parser_10_3 const &) = delete;
+};
+
+#endif
