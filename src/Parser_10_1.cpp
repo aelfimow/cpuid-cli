643c878681f31979443f722e8d29538e23a1c315
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-24 22:28:10 +0100

    Parser_10_1: implemented parsing

--- src/Parser_10_1.cpp
+++ src/Parser_10_1.cpp
@@ -2,6 +2,7 @@
 #include "Parser_10_1.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_10_1::Parser_10_1(cpuid_response const &data) :
@@ -37,19 +38,48 @@ parse_result_t Parser_10_1::parse() const
 void Parser_10_1::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Length of the capacity bit mask",
+        1 + extr.extract(4, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_10_1::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Bit-granular map of isolation/contention of allocation units",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_10_1::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    if (extr.extract(2))
+    {
+        m_result.push_back("Code and Data Prioritization Technology supported");
+    }
 }
 
 void Parser_10_1::parseRDX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Highest COS number supported for ResID 1",
+        extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
