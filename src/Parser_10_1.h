f73d29dfaa507907d3b2bf5dd21e67b903ef7f3c
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-24 22:17:12 +0100

    Parser_10_1: created

---
+++ src/Parser_10_1.h
@@ -0,0 +1,31 @@
+#ifdef PARSER_10_1_H
+#error Already included
+#else
+#define PARSER_10_1_H
+
+class cpuid_response;
+
+class Parser_10_1 : public IParser
+{
+    public:
+        explicit Parser_10_1(cpuid_response const &data);
+        ~Parser_10_1();
+
+        parse_result_t parse() const override final;
+
+    private:
+        parse_result_t m_result;
+        IParser *m_next;
+
+        void parseRAX(size_t value);
+        void parseRBX(size_t value);
+        void parseRCX(size_t value);
+        void parseRDX(size_t value);
+
+    public:
+        Parser_10_1() = delete;
+        Parser_10_1(Parser_10_1 const &) = delete;
+        Parser_10_1 &operator=(Parser_10_1 const &) = delete;
+};
+
+#endif
