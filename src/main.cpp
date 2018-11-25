3293014281ee4f971bc6d63b5e165c2b3aab9a90
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-25 20:04:20 +0100

    Use ParserDispatcher now

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,9 @@
 #include <map>
 #include <memory>
 #include <vector>
-#include <functional>
 
+#include "ParserDispatcher.h"
 #include "IParser.h"
-#include "Parser_0_0.h"
-#include "Parser_1_0.h"
-#include "Parser_2_0.h"
-#include "Parser_3_0.h"
-#include "Parser_4_0.h"
-#include "Parser_5_0.h"
-#include "Parser_6_0.h"
-#include "Parser_7_0.h"
-#include "Parser_9_0.h"
-#include "Parser_A_0.h"
-#include "Parser_B_0.h"
-#include "Parser_D_0.h"
-#include "Parser_F_0.h"
-#include "Parser_10_0.h"
 #include "cpuid_response.h"
 #include "cpuid_func.h"
 
@@ -59,34 +45,18 @@ try
 
     std::cout << response.str() << std::endl;
 
-    std::map<size_t, std::function<IParser *(cpuid_response const &)>> const factory
+    std::unique_ptr<IParser> parser(ParserDispatcher::Parser_for(response));
+
+    if (!parser)
     {
-        { 0x00, [](cpuid_response const &d) { return new Parser_0_0(d); } },
-        { 0x01, [](cpuid_response const &d) { return new Parser_1_0(d); } },
-        { 0x02, [](cpuid_response const &d) { return new Parser_2_0(d); } },
-        { 0x03, [](cpuid_response const &d) { return new Parser_3_0(d); } },
-        { 0x04, [](cpuid_response const &d) { return new Parser_4_0(d); } },
-        { 0x05, [](cpuid_response const &d) { return new Parser_5_0(d); } },
-        { 0x06, [](cpuid_response const &d) { return new Parser_6_0(d); } },
-        { 0x07, [](cpuid_response const &d) { return new Parser_7_0(d); } },
-        { 0x09, [](cpuid_response const &d) { return new Parser_9_0(d); } },
-        { 0x0A, [](cpuid_response const &d) { return new Parser_A_0(d); } },
-        { 0x0B, [](cpuid_response const &d) { return new Parser_B_0(d); } },
-        { 0x0D, [](cpuid_response const &d) { return new Parser_D_0(d); } },
-        { 0x0F, [](cpuid_response const &d) { return new Parser_F_0(d); } },
-        { 0x10, [](cpuid_response const &d) { return new Parser_10_0(d); } }
-    };
+        throw std::invalid_argument("No parser.");
+    }
 
-    if (auto f = factory.find(RAX_value); f != factory.end())
+    auto result { parser->parse() };
+
+    for (auto str: result)
     {
-        std::unique_ptr<IParser> parser(f->second(response));
-
-        auto result { parser->parse() };
-
-        for (auto str: result)
-        {
-            std::cout << str << std::endl;
-        }
+        std::cout << str << std::endl;
     }
 
     return EXIT_SUCCESS;
