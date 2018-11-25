75208da2b49d7534c730996a3e5678f0dcc70e9c
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-25 20:34:51 +0100

    Parser_12_0: implemented parsing

--- src/Parser_12_0.cpp
+++ src/Parser_12_0.cpp
@@ -1,8 +1,11 @@
+#include <bitset>
+
 #include "IParser.h"
 #include "Parser_12_0.h"
 #include "Parser_12_1.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_12_0::Parser_12_0(cpuid_response const &data) :
@@ -42,11 +45,25 @@ parse_result_t Parser_12_0::parse() const
 void Parser_12_0::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    if (extr.extract(0))
+    {
+        m_result.push_back("SGX1 leaf functions supported");
+    }
+
+    if (extr.extract(1))
+    {
+        m_result.push_back("SGX2 leaf functions supported");
+    }
 }
 
 void Parser_12_0::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    std::bitset<32> bits { extr.extract(31, 0) };
+
+    m_result.push_back(bits.to_string());
 }
 
 void Parser_12_0::parseRCX(size_t value)
@@ -57,4 +74,28 @@ void Parser_12_0::parseRCX(size_t value)
 void Parser_12_0::parseRDX(size_t value)
 {
     bit_extractor extr { value };
+
+    {
+        size_t size = (1 << extr.extract(7, 0));
+
+        ParserString pstr
+        {
+            "Maximum supported enclave size in non-64-bit mode",
+            size
+        };
+
+        m_result.push_back(pstr.str());
+    }
+
+    {
+        size_t size = (1 << extr.extract(15, 8));
+
+        ParserString pstr
+        {
+            "Maximum supported enclave size in 64-bit mode",
+            size
+        };
+
+        m_result.push_back(pstr.str());
+    }
 }
