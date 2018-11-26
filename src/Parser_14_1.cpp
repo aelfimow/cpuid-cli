2f9de0515678b42ba15d656f2bdc3fb85ba8d3f9
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-26 22:48:01 +0100

    Parser_14_1: implemented parsing

--- src/Parser_14_1.cpp
+++ src/Parser_14_1.cpp
@@ -1,7 +1,10 @@
+#include <bitset>
+
 #include "IParser.h"
 #include "Parser_14_1.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_14_1::Parser_14_1(cpuid_response const &data) :
@@ -37,11 +40,57 @@ parse_result_t Parser_14_1::parse() const
 void Parser_14_1::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    {
+        ParserString pstr
+        {
+            "Number of configurable Address Ranges for filtering",
+            extr.extract(2, 0)
+        };
+
+        m_result.push_back(pstr.str());
+    }
+
+    {
+        std::bitset<16> bits { extr.extract(31, 16) };
+
+        ParserString pstr
+        {
+            "Bitmap of supported MTC period encodings",
+            bits.to_string()
+        };
+
+        m_result.push_back(pstr.str());
+    }
 }
 
 void Parser_14_1::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    {
+        std::bitset<16> bits { extr.extract(15, 0) };
+
+        ParserString pstr
+        {
+            "Bitmap of supported Cycle Threshold value encodings",
+            bits.to_string()
+        };
+
+        m_result.push_back(pstr.str());
+    }
+
+    {
+        std::bitset<16> bits { extr.extract(31, 16) };
+
+        ParserString pstr
+        {
+            "Bitmap of supported Configurable PSB frequency encodings",
+            bits.to_string()
+        };
+
+        m_result.push_back(pstr.str());
+    }
 }
 
 void Parser_14_1::parseRCX(size_t value)
