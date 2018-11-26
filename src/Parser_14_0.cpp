2a6cc810c3b48312e033d8cf0da968733513dd56
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-26 22:40:58 +0100

    Parser_14_0: implemented parsing

--- src/Parser_14_0.cpp
+++ src/Parser_14_0.cpp
@@ -1,8 +1,12 @@
+#include <string>
+#include <map>
+
 #include "IParser.h"
 #include "Parser_14_0.h"
 #include "Parser_14_1.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_14_0::Parser_14_0(cpuid_response const &data) :
@@ -42,16 +46,59 @@ parse_result_t Parser_14_0::parse() const
 void Parser_14_0::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Maximum sub-leaf",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_14_0::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    std::map<size_t, std::string> const table
+    {
+        { 0, "IA32_RTIT_CTL.CR3Filter can be set to 1; IA32_RTIT_CR3_MATCH MSR can be accessed" },
+        { 1, "Configurable PSB and Cycle-Accurate Mode supported" },
+        { 2, "IP/TraceStop filtering and preservation of Intel PT MSRs across warm reset supported" },
+        { 3, "MTC timing packet and suppression of COFI-based packets supported" },
+        { 4, "PTWRITE supported" },
+        { 5, "Power Event Trace supported" }
+    };
+
+    for (auto &t: table)
+    {
+        if (extr.extract(t.first))
+        {
+            m_result.push_back(t.second);
+        }
+    }
 }
 
 void Parser_14_0::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    std::map<size_t, std::string> const table
+    {
+        { 0, "Tracing can be enabled" },
+        { 1, "ToPA tables can hold any number of output entries" },
+        { 2, "Single-Range Output scheme supported" },
+        { 3, "Output to Trace Transport subsystem supported" },
+        { 31, "Generated packets which contain IP payloads have LIP values, which include the CS base component" }
+    };
+
+    for (auto &t: table)
+    {
+        if (extr.extract(t.first))
+        {
+            m_result.push_back(t.second);
+        }
+    }
 }
 
 void Parser_14_0::parseRDX(size_t value)
