d155716f9dc07b2bfdc7621edac30323cbe1a96d
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-27 22:24:25 +0100

    Parser_15_0: implemented parsing

--- src/Parser_15_0.cpp
+++ src/Parser_15_0.cpp
@@ -2,6 +2,7 @@
 #include "Parser_15_0.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_15_0::Parser_15_0(cpuid_response const &data) :
@@ -25,16 +26,40 @@ parse_result_t Parser_15_0::parse() const
 void Parser_15_0::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Denominator of the TSC/'core crystal clock' ratio",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_15_0::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Numerator of the TSC/'core crystal clock' ratio",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_15_0::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Nominal frequency of the core crystal clock (in Hz)",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_15_0::parseRDX(size_t value)
