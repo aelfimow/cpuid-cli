be961cb818dd25a8f265f9923ad07d4d1dd21d68
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-28 22:33:39 +0100

    Parser_16_0: implemented parsing

--- src/Parser_16_0.cpp
+++ src/Parser_16_0.cpp
@@ -2,6 +2,7 @@
 #include "Parser_16_0.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_16_0::Parser_16_0(cpuid_response const &data) :
@@ -25,16 +26,37 @@ parse_result_t Parser_16_0::parse() const
 void Parser_16_0::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Processor Base Frequency (in MHz)", extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_16_0::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Maximum Frequency (in MHz)", extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_16_0::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Bus (Reference) Frequency (in MHz)", extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_16_0::parseRDX(size_t value)
