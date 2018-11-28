d22a230fa895c52fc79bd9b34b5a08e39f652dcc
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-28 22:27:19 +0100

    Parser_16_0: created

---
+++ src/Parser_16_0.h
@@ -0,0 +1,28 @@
+#ifdef PARSER_16_0_H
+#error Already included
+#else
+#define PARSER_16_0_H
+
+class cpuid_response;
+
+class Parser_16_0 : public IParser
+{
+    public:
+        explicit Parser_16_0(cpuid_response const &data);
+        ~Parser_16_0();
+
+        parse_result_t parse() const override final;
+
+    private:
+        parse_result_t m_result;
+
+        void parseRAX(size_t value);
+        void parseRBX(size_t value);
+        void parseRCX(size_t value);
+        void parseRDX(size_t value);
+
+    public:
+        Parser_16_0() = delete;
+};
+
+#endif
