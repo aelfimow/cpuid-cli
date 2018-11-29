79eea5e0f707440ff092109ec14b87e4fd23598f
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-29 22:08:50 +0100

    Parser_Ext_0: created

---
+++ src/Parser_Ext_0.cpp
@@ -0,0 +1,52 @@
+#include "IParser.h"
+#include "Parser_Ext_0.h"
+#include "bit_extractor.h"
+#include "cpuid_response.h"
+#include "ParserString.h"
+
+
+Parser_Ext_0::Parser_Ext_0(cpuid_response const &data) :
+    m_result { }
+{
+    parseRAX(data.RAX());
+    parseRBX(data.RBX());
+    parseRCX(data.RCX());
+    parseRDX(data.RDX());
+}
+
+Parser_Ext_0::~Parser_Ext_0()
+{
+}
+
+parse_result_t Parser_Ext_0::parse() const
+{
+    return m_result;
+}
+
+void Parser_Ext_0::parseRAX(size_t value)
+{
+    bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Maximum Input Value for Extended Function CPUID Information",
+        extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
+}
+
+void Parser_Ext_0::parseRBX(size_t value)
+{
+    bit_extractor extr { value };
+}
+
+void Parser_Ext_0::parseRCX(size_t value)
+{
+    bit_extractor extr { value };
+}
+
+void Parser_Ext_0::parseRDX(size_t value)
+{
+    bit_extractor extr { value };
+}
---
+++ src/Parser_Ext_0.h
@@ -0,0 +1,28 @@
+#ifdef PARSER_EXT_0_H
+#error Already included
+#else
+#define PARSER_EXT_0_H
+
+class cpuid_response;
+
+class Parser_Ext_0 : public IParser
+{
+    public:
+        explicit Parser_Ext_0(cpuid_response const &data);
+        ~Parser_Ext_0();
+
+        parse_result_t parse() const override final;
+
+    private:
+        parse_result_t m_result;
+
+        void parseRAX(size_t value);
+        void parseRBX(size_t value);
+        void parseRCX(size_t value);
+        void parseRDX(size_t value);
+
+    public:
+        Parser_Ext_0() = delete;
+};
+
+#endif
