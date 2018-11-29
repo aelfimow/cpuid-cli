d782a2a0dd36eace276b33773ff01c49d68a00fe
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-29 21:40:28 +0100

    Parser_17_0: implemented parsing

--- src/Parser_17_0.cpp
+++ src/Parser_17_0.cpp
@@ -3,6 +3,7 @@
 #include "Parser_17_1.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_17_0::Parser_17_0(cpuid_response const &data) :
@@ -42,19 +43,52 @@ parse_result_t Parser_17_0::parse() const
 void Parser_17_0::parseRAX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Maximum input value of supported sub-leaf", extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_17_0::parseRBX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "SOC Vendor ID", extr.extract(15, 0)
+    };
+
+    m_result.push_back(pstr.str());
+
+    m_result.push_back(
+            extr.extract(16) ?
+            "SOC Vendor ID field is assigned via an industry standard enumeration scheme" :
+            "SOC Vendor ID field is assigned by Intel");
 }
 
 void Parser_17_0::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Project ID", extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
 
 void Parser_17_0::parseRDX(size_t value)
 {
     bit_extractor extr { value };
+
+    ParserString pstr
+    {
+        "Stepping ID", extr.extract(31, 0)
+    };
+
+    m_result.push_back(pstr.str());
 }
