16058674152db20503a02a080099b1aa762593d5
Alexander Elfimow <my.free.mail@gmx.de>
2018-12-02 20:06:18 +0100

    Parser_Ext_6: implemented parsing

--- src/Parser_Ext_6.cpp
+++ src/Parser_Ext_6.cpp
@@ -1,7 +1,11 @@
+#include <string>
+#include <map>
+
 #include "IParser.h"
 #include "Parser_Ext_6.h"
 #include "bit_extractor.h"
 #include "cpuid_response.h"
+#include "ParserString.h"
 
 
 Parser_Ext_6::Parser_Ext_6(cpuid_response const &data) :
@@ -35,6 +39,47 @@ void Parser_Ext_6::parseRBX(size_t value)
 void Parser_Ext_6::parseRCX(size_t value)
 {
     bit_extractor extr { value };
+
+    {
+        ParserString pstr
+        {
+            "Cache Line size in bytes",
+                extr.extract(7, 0)
+        };
+
+        m_result.push_back(pstr.str());
+    }
+
+    std::map<size_t, std::string> const table
+    {
+        { 0x00, "Disabled" },
+        { 0x01, "Direct mapped" },
+        { 0x02, "2-way" },
+        { 0x04, "4-way" },
+        { 0x06, "8-way" },
+        { 0x08, "16-way" },
+        { 0x0F, "Fully associative" }
+    };
+
+    for (auto &t: table)
+    {
+        if (t.first == extr.extract(15, 12))
+        {
+            ParserString pstr { "L2 Associativity", t.second };
+
+            m_result.push_back(pstr.str());
+        }
+    }
+
+    {
+        ParserString pstr
+        {
+            "Cache size in 1K units",
+            extr.extract(31, 16)
+        };
+
+        m_result.push_back(pstr.str());
+    }
 }
 
 void Parser_Ext_6::parseRDX(size_t value)
