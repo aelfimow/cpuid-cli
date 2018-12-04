6a6b329b84e50e3f3a900c13f61803f31a5c27fc
Alexander Elfimow <my.free.mail@gmx.de>
2018-12-04 22:16:44 +0100

    ParserString: extended functionality

--- src/ParserString.cpp
+++ src/ParserString.cpp
@@ -3,6 +3,11 @@
 #include "ParserString.h"
 
 
+ParserString::ParserString() :
+    m_str { }
+{
+}
+
 ParserString::ParserString(std::string const str, size_t value) :
     m_str { }
 {
@@ -21,6 +26,33 @@ ParserString::ParserString(std::string const str1, std::string const str2) :
 
 ParserString::~ParserString()
 {
+}
+
+ParserString &ParserString::clear()
+{
+    m_str.clear();
+    return *this;
+}
+
+ParserString &ParserString::prefix(std::string const &str)
+{
+    m_str = str;
+    m_str.append(": ");
+    return *this;
+}
+
+ParserString &ParserString::append(size_t value)
+{
+    std::stringstream ss;
+    ss << value;
+    m_str.append(ss.str());
+    return *this;
+}
+
+ParserString &ParserString::append(std::string const &str)
+{
+    m_str.append(str);
+    return *this;
 }
 
 std::string ParserString::str() const
--- src/ParserString.h
+++ src/ParserString.h
@@ -6,17 +6,22 @@
 class ParserString
 {
     public:
+        ParserString();
         explicit ParserString(std::string const str, size_t value);
         explicit ParserString(std::string const str1, std::string const str2);
         ~ParserString();
+
+        ParserString &clear();
+
+        ParserString &prefix(std::string const &str);
+
+        ParserString &append(size_t value);
+        ParserString &append(std::string const &str);
 
         std::string str() const;
 
     private:
         std::string m_str;
-
-    public:
-        ParserString() = delete;
 };
 
 #endif
