b83f4b9ed6d744282a19f0b44c450b43945d7eb6
Alexander Elfimow <my.free.mail@gmx.de>
2019-01-02 22:51:06 +0100

    Parser_Ext_1: moved parsing to parse()

--- src/Parser_Ext_1.h
+++ src/Parser_Ext_1.h
@@ -14,6 +14,11 @@ class Parser_Ext_1 : public IParser
         parse_result_t parse() override final;
 
     private:
+        size_t const m_RAX;
+        size_t const m_RBX;
+        size_t const m_RCX;
+        size_t const m_RDX;
+
         parse_result_t m_result;
 
         void parseRAX(size_t value);
