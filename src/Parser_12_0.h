e9c687c80a933a02f095da3901c3c9d1577750e8
Alexander Elfimow <my.free.mail@gmx.de>
2019-01-06 21:50:52 +0100

    Parser_12_0: moved parsing to parse()

--- src/Parser_12_0.h
+++ src/Parser_12_0.h
@@ -14,6 +14,11 @@ class Parser_12_0 : public IParser
         parse_result_t parse() override final;
 
     private:
+        size_t const m_RAX;
+        size_t const m_RBX;
+        size_t const m_RCX;
+        size_t const m_RDX;
+
         parse_result_t m_result;
         IParser *m_next;
 
