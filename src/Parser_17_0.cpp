e68ecbac9eb7dcd69bb29aee0ed834761c2a5905
Alexander Elfimow <my.free.mail@gmx.de>
2019-01-06 22:05:59 +0100

    Parser_17_0: moved parsing to parse()

--- src/Parser_17_0.cpp
+++ src/Parser_17_0.cpp
@@ -7,22 +7,13 @@
 
 
 Parser_17_0::Parser_17_0(cpuid_response const &data) :
+    m_RAX { data.RAX() },
+    m_RBX { data.RBX() },
+    m_RCX { data.RCX() },
+    m_RDX { data.RDX() },
     m_result { },
-    m_next { nullptr }
+    m_next { (0 == data.RCX_Command()) ? nullptr : new Parser_17_1 { data } }
 {
-    bool response_ok = (0 == data.RCX_Command());
-
-    if (response_ok)
-    {
-        parseRAX(data.RAX());
-        parseRBX(data.RBX());
-        parseRCX(data.RCX());
-        parseRDX(data.RDX());
-    }
-    else
-    {
-        m_next = new Parser_17_1 { data };
-    }
 }
 
 Parser_17_0::~Parser_17_0()
@@ -36,6 +27,13 @@ parse_result_t Parser_17_0::parse()
     {
         return m_next->parse();
     }
+
+    m_result.clear();
+
+    parseRAX(m_RAX);
+    parseRBX(m_RBX);
+    parseRCX(m_RCX);
+    parseRDX(m_RDX);
 
     return m_result;
 }
