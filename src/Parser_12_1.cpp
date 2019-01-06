0332457785934311d9da7dad63a8474c171b2123
Alexander Elfimow <my.free.mail@gmx.de>
2019-01-06 21:53:30 +0100

    Parser_12_1: moved parsing to parse()

--- src/Parser_12_1.cpp
+++ src/Parser_12_1.cpp
@@ -9,22 +9,13 @@
 
 
 Parser_12_1::Parser_12_1(cpuid_response const &data) :
+    m_RAX { data.RAX() },
+    m_RBX { data.RBX() },
+    m_RCX { data.RCX() },
+    m_RDX { data.RDX() },
     m_result { },
-    m_next { nullptr }
+    m_next { (1 == data.RCX_Command()) ? nullptr : new Parser_12_2 { data } }
 {
-    bool response_ok = (1 == data.RCX_Command());
-
-    if (response_ok)
-    {
-        parseRAX(data.RAX());
-        parseRBX(data.RBX());
-        parseRCX(data.RCX());
-        parseRDX(data.RDX());
-    }
-    else
-    {
-        m_next = new Parser_12_2 { data };
-    }
 }
 
 Parser_12_1::~Parser_12_1()
@@ -38,6 +29,13 @@ parse_result_t Parser_12_1::parse()
     {
         return m_next->parse();
     }
+
+    m_result.clear();
+
+    parseRAX(m_RAX);
+    parseRBX(m_RBX);
+    parseRCX(m_RCX);
+    parseRDX(m_RDX);
 
     return m_result;
 }
